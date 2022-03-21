b9daed084b14e69d52266bbbb89e14e75bbab6cf
Sudarshan <mail2sudarshan.sharma@gmail.com>
2022-03-21 11:56:23 +0530

    first set of development

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi assumenda quis in laborum voluptates ipsam, enim, natus ipsum culpa explicabo quia? Minima, consequuntur eveniet dolorum quos minus quae ea explicabo, quisquam fugit facilis, quod cumque quam magni. Aliquid, voluptatem?
+      </p>
+    </section>
+  )
+}
+
+export default About
